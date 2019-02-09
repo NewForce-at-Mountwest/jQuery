81f05be69d4f2225f7b6aa878bc297d76e2241de
Jordan Castelloe <jordan.castelloe@gmail.com>
2019-02-09 12:24:30 -0500

    demo code for jquery

---
+++ lightning.js
@@ -0,0 +1,23 @@
+// Add the jQuery CDN to your HTML file
+
+
+// ------- ONE ------//
+// Copy and paste the following HTML into your index.html file:
+// <article id="blog-article"></article>
+
+// Use jQuery to select your article element by its id
+
+// Use the .html() method to give your article Element the following HTML contents:
+// - An h3  element with a made-up blog title
+// - A p element with some lorem ipsum text
+
+
+// ------ TWO ------//
+// Copy and paste the following code into your HTML file:
+// <input type="text" id="name-input">
+// <button id="submit-button"></button>
+// <div id="outout"></div>
+
+// Add a click event to the button. When the user clicks on it, use the .val() method to get the value of the #name-input element
+
+// Once you've captured the user's input, use the .text() property to add some text to the #output div
---
+++ script.js
@@ -0,0 +1,31 @@
+// // What is jQuery?
+// // How do I access it?
+
+
+// Selecting an element with vanilla JS
+
+// Selecting an element with jQuery
+
+// Inserting HTML string with vanilla JavaScript
+
+// Inserting HTML string with jQuery
+
+// Click events with vanilla JS
+
+// Click events with jQuery
+
+// Creating elements with vanilla JS
+
+// Creating elements with jQuery
+
+//Adding classes and ids with vanilla JS
+
+// Adding classes and ids with jQuery
+
+// Appending with vanilla JS
+
+// Appending with jQuery
+
+
+
+
